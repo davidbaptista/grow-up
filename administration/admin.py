9cf21373f1be6e5bd1968b7e2dc443203a2a2922
David Baptista <david.baptista00@hotmail.com>
2020-02-11 16:06:58 +0000

    passar para portatil

--- administration/admin.py
+++ administration/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
 
-from dashboard.models import OrganisationProfile
+from dashboard.models import OrganisationProfile, VolunteerProfile
 
 admin.site.unregister(Group)
 
@@ -9,5 +9,9 @@ admin.site.unregister(Group)
 class OrganisationProfileAdmin(admin.ModelAdmin):
 	pass
 
+class VolunteerProfileAdmin(admin.ModelAdmin):
+	pass
+
 
 admin.site.register(OrganisationProfile, OrganisationProfileAdmin)
+admin.site.register(VolunteerProfile, VolunteerProfileAdmin)
